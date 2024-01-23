025f478a21eac6819a2f6b2352b232bbb1a70570
nozipho969 <147490645+nozipho969@users.noreply.github.com>
2024-01-23 22:08:47 +0200

    Added basic search engine event.

--- Source/index.js
+++ Source/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function handleTextSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#weather-city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let textFormElement = document.querySelector("#search-form");
+textFormElement.addEventListener("submit", handleTextSubmit);

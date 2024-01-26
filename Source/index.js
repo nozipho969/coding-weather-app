c791d4e9f3d57d010f5f6160bda04462555bd973
nozipho969 <147490645+nozipho969@users.noreply.github.com>
2024-01-26 09:48:50 +0200

    Injected current time data as well as feels-like description

--- Source/index.js
+++ Source/index.js
@@ -5,14 +5,30 @@ function displayWeather(response) {
   let conditionElement = document.querySelector("#condition");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let feelsLikeElement = document.querySelector("#feels-like");
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time *1000);
 
-  console.log(response.data.temperature.windspeed);
+  console.log(response.data);
 
   cityElement.innerHTML = response.data.city;
+
+  timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()};
   conditionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  feelsLikeElement.innerHTML = `${response.data.temperature.feels_like}°`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formateDate(date) {
+let day = date.getDay();
+let minutes = date.getMinutes();
+let hours = date.getHours();
+
+
+let days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
+
 }
 
 function checkOwnCity(city) {
